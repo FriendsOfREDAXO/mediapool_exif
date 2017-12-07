f5cc816b38f7d3703d36182ab720f55687cbe4bb
Tony Münzberg <tony@muenzberg.me>
2017-12-07 14:37:16 +0100

    Anlegen der Metainfo Felder

---
+++ install.php
@@ -0,0 +1,7 @@
+<?php
+rex_metainfo_add_field('Beschreibung', 'med_description', '1', '', '2', '', '', '', '');
+rex_metainfo_add_field('Schlagworte', 'med_keywords', '2', '', '1', '', '', '', '');
+rex_metainfo_add_field('Copyright', 'med_copyright', '3', '', '1', '', '', '', '');
+rex_metainfo_add_field('Autor', 'med_author', '4', '', '1', '', '', '', '');
+rex_metainfo_add_field('Ausrichtung', 'med_orientation', '5', '', '1', '', '', '', '');
+rex_metainfo_add_field('Erstellungsdatum', 'med_createdate', '6', '', '1', '', '', '', '');
---
+++ uninstall.php
@@ -0,0 +1,8 @@
+<?php
+
+rex_metainfo_delete_field('med_description');
+rex_metainfo_delete_field('med_copyright');
+rex_metainfo_delete_field('med_author');
+rex_metainfo_delete_field('med_orientation');
+rex_metainfo_delete_field('med_createdate');
+rex_metainfo_delete_field('med_keywords');
