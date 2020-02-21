8311b45a27309cf65311dade136adbe7e4fd5035
Alexander Walther <github.com@alexplus.de>
2020-02-21 21:01:10 +0100

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,7 +1,1 @@
 <?php
-rex_metainfo_delete_field('med_description');
-rex_metainfo_delete_field('med_copyright');
-rex_metainfo_delete_field('med_author');
-rex_metainfo_delete_field('med_orientation');
-rex_metainfo_delete_field('med_createdate');
-rex_metainfo_delete_field('med_keywords');
