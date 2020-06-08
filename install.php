fb76e8dcacdd10650651f9164e990782ff983464
Axel Krysztofiak <akrys@web.de>
2020-06-08 17:50:48 +0200

    Update install.php
    Typo

--- install.php
+++ install.php
@@ -11,4 +11,4 @@ rex_metainfo_add_field('Copyright', 'med_copyright', '3', '', '1', '', '', '', '
 rex_metainfo_add_field('Autor', 'med_author', '4', '', '1', '', '', '', '');
 rex_metainfo_add_field('Ausrichtung', 'med_orientation', '5', '', '1', '', '', '', '');
 rex_metainfo_add_field('Erstellungsdatum', 'med_createdate', '6', '', '1', '', '', '', '');
-rex_metainfo_add_field('Geo-koordinaten', 'med_gps', '7', '', '1', '', '', '', '');
+rex_metainfo_add_field('Geo-Koordinaten', 'med_gps', '7', '', '1', '', '', '', '');
