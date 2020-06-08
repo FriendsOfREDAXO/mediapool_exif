5757d5014c487df3df4271471c679927ab1dd82f
Axel Krysztofiak <akrys@web.de>
2020-06-08 16:56:06 +0200

    - Datenabfrage einbauen - CLI-Command um die fehlenden EXIF-Daten einzulesen (auch alle neu einlesen)

---
+++ lib/Exception/NotFoundException.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Datei für ...
+ *
+ * @version       1.0 / 2020-06-08
+ * @author        akrys
+ */
+namespace FriendsOfRedaxo\addon\MediapoolExif\Exception;
+
+/**
+ * Description of NotFoundException
+ *
+ * @author akrys
+ */
+class NotFoundException
+	extends \Exception
+{
+	/**
+	 *
+	 * @todo activate type hint if min PHP-Version > 7.4
+	 * @var string
+	 */
+	public /* string */ $index;
+
+	/**
+	 * Konstruktor
+	 * @param string $index
+	 * @param string $message
+	 * @param int $code
+	 * @param \Throwable $previous
+	 */
+	public function __construct(string $index, string $message = "", int $code = 0, \Throwable $previous = NULL)
+	{
+		$this->index = $index;
+		parent::__construct($message, $code, $previous);
+	}
+
+	/**
+	 * Index alleine holen.
+	 * Damit kann man in einem Catch-Block eigene Infos zusammenstellen.
+	 * Was der Fehler ist, ergibt sich ja aus dem Exception-Namen.
+	 * @return string
+	 */
+	public function getIndex(): string
+	{
+		return $this->index;
+	}
+}
---
+++ lib/ExifData.php
@@ -0,0 +1,136 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace FriendsOfRedaxo\addon\MediapoolExif;
+
+use FriendsOfRedaxo\addon\MediapoolExif\Exception\NotFoundException;
+use rex_media;
+
+/**
+ * Datei für ...
+ *
+ * @version       1.0 / 2020-06-08
+ * @author        akrys
+ */
+
+/**
+ * Description of ExifData
+ *
+ * @author akrys
+ */
+class ExifData
+{
+	/**
+	 * Media-Objekt
+	 *
+	 * @todo activate type hint if min PHP-Version > 7.4
+	 * @var rex_media
+	 */
+	private /* rex_media */ $media;
+
+	/**
+	 * Modus
+	 * @var int
+	 */
+	private /* int */ $mode;
+
+	/**
+	 * Konstruktor
+	 *
+	 * Modus für die Fehlerbehandlung
+	 * Standard: MODE_THROW_EXCEPTION
+	 *
+	 * Wer keine try/catch-Blocke mag, kann sich in dem Fall dann andere false-Werte liefern lassen.
+	 *
+	 * Die Gefahr, dass Code-Technisch nicht mehr erkannt werden kann, ob es ein Fehler gab oder ob der Wert
+	 * tatsächlich 0 oder false oder was auch immer ist, liegt dann natürlich beim jeweiligen Entwickler.
+	 * Garantierte Eindeutigkeit gibt es nur im Modus MODE_THROW_EXCEPTION. (Darum auch der Standard)
+	 *
+	 * <ul>
+	 * <li>false (MODE_RETURN_FALSE)</li>
+	 * <li>null (MODE_RETURN_NULL)</li>
+	 * <li>0 (MODE_RETURN_ZERO)</li>
+	 * <li>-1 (MODE_RETURN_MINUS)</li>
+	 * <li>'' (MODE_RETURN_EMPTY_STRING)</li>
+	 * <li>[] (MODE_RETURN_EMPTY_ARRAY)</li>
+	 * </ol>
+	 *
+	 * @param \FriendsOfRedaxo\addon\MediapoolExif\rex_media $media
+	 * @param int $mode
+	 */
+	public function __construct(rex_media $media, int $mode = null)
+	{
+		$this->media = $media;
+
+		if ($mode === null) {
+			$mode = Exif::MODE_THROW_EXCEPTION;
+		}
+		$this->mode = $mode;
+	}
+
+	/**
+	 * Daten holen
+	 *
+	 * Ist der Index nicht gesetzt, kommt alles in Form eines Arrays zurück.
+	 *
+	 * @param string $index
+	 * @return mixed
+	 * @throws NotFoundException
+	 */
+	public function get(string $index = null)
+	{
+		$data = json_decode($this->media->getValue('exif'), true);
+
+		if ($index !== null) {
+			if (!array_key_exists($index, $data)) {
+				return $this->handleNotFound($index);
+			}
+			return $data[$index];
+		}
+
+		return $data;
+	}
+
+	/**
+	 * Fehler-Behandlung
+	 *
+	 * Welche Rückgabe hätten's gern?
+	 *
+	 * @param string $index
+	 * @return mixed
+	 * @throws NotFoundException
+	 */
+	private function handleNotFound(string $index)
+	{
+		$return = '';
+
+		switch ($this->mode) {
+			case Exif::MODE_RETURN_NULL:
+				$return = null;
+				break;
+			case Exif::MODE_RETURN_FALSE:
+				$return = false;
+				break;
+			case Exif::MODE_RETURN_ZERO:
+				$return = 0;
+				break;
+			case Exif::MODE_RETURN_MINUS:
+				$return = -1;
+				break;
+			case Exif::MODE_RETURN_EMPTY_STRING:
+				$return = '';
+				break;
+			case Exif::MODE_RETURN_EMPTY_ARRAY:
+				$return = [];
+				break;
+			case Exif::MODE_THROW_EXCEPTION:
+			default:
+				throw new NotFoundException($index, 'Index not found: '.$index);
+		}
+		return $return;
+	}
+}
