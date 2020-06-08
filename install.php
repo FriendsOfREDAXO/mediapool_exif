b9ed7500bf587b00a7233a7c5a610c5874d1a856
Axel Krysztofiak <akrys@web.de>
2020-06-08 08:16:12 +0200

    Umbenennung nicht da?

--- install.php
+++ install.php
@@ -2,6 +2,6 @@
 
 rex_sql_table::get(rex::getTable('media'))
     ->ensurePrimaryIdColumn()
-    ->ensureColumn(new rex_sql_column('exif_json', 'longtext', true))
+    ->ensureColumn(new rex_sql_column('exif', 'longtext', true))
     ->ensure();
 
